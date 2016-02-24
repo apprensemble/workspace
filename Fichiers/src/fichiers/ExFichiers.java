4e29f4ad61ff4b25c5b880201496a522e65cb6f2
thierry@moncourriel.eu <thierry>
2016-02-24 14:28:43 +0100

    Les Fichiers
    Suite a des pb avec git j'ai du faire du git repair au lieu de coder la
    loose.
    Pour info j'étais en train de commiter lorsque la batterie a laché.
    resultat index git corrompu et un objet manquant au git fsck.

--- Fichiers/src/fichiers/ExFichiers.java
+++ Fichiers/src/fichiers/ExFichiers.java
@@ -1,8 +1,36 @@
 package fichiers;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.function.Consumer;
+
+import outils.Pres;
 
 public class ExFichiers {
 	public static void main(String[] args) {
-		outils.Pres.titre("Les Fichiers");
-	
+		Pres.titre("Les Fichiers");
+		Path image = Paths.get("src/ressources/boot_menu_w.png");
+		Path res = Paths.get("src/res/nimage.png");
+		Consumer<Path> fichierExiste = f -> System.out.println(Files.exists(f) ? f+" existe" : f+" n'existe pas"); 
+		try {
+			fichierExiste.accept(res);
+			Files.deleteIfExists(res);
+			Files.deleteIfExists(res.getParent());
+			Files.createDirectory(res.getParent());
+			fichierExiste.accept(res);
+		}
+		catch (IOException e) {
+			Pres.titre("erreur mkdir");
+			e.printStackTrace();
+		}
+		try {
+			Files.copy(image,res);
+			fichierExiste.accept(res);
+		}
+		catch (IOException e) {
+			Pres.titre("erreur copy");
+			e.printStackTrace();
+		}
 	}
 }
