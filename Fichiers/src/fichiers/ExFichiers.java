904a08688c30d0a606fa76f6b611f46680d7245a
thierry@moncourriel.eu <thierry>
2016-02-26 07:44:05 +0100

    Fin des fichiers
    Je n'ai pas bien compris les scanners. Je trouve ca difficile a
    manoeuvrer.
    J'aimerais bien pouvoir iterer sur des scanner avec un foreach plutot
    qu'avec des strings...

--- Fichiers/src/fichiers/ExFichiers.java
+++ Fichiers/src/fichiers/ExFichiers.java
@@ -1,5 +1,9 @@
 package fichiers;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,6 +14,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.Scanner;
 import java.util.SortedMap;
 import java.util.StringJoiner;
 import java.util.TreeMap;
@@ -18,6 +23,8 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import couple.Couple;
 
 import outils.Pres;
 
@@ -29,6 +36,8 @@ public class ExFichiers {
 	private static Consumer<Path> fichierExiste = f -> System.out.println(Files.exists(f) ? f+" existe" : f+" n'existe pas"); 
 	private static Comparator<String> cmpStr = (v1,v2) -> v1.compareTo(v2);
 	private static Optional<List<String>> lr;
+	private static Path source = Paths.get("src/ressources/varNombre.txt");
+	private static Path objetCouple = Paths.get("src/ressources/objetCouple.dat");
 
 	private static void net() {
 		fichierExiste.accept(res);
@@ -99,9 +108,25 @@ public class ExFichiers {
 		Pres.titre("exercice4");
 		//deux maniere de capture les elements dans un fichier :
 		//1 - ligne par ligne puis on split
-		//2 - caractere par caractere avec des conditions
-		Path source = Paths.get("src/ressources/varNombre.txt");
+		//2 - ligne par ligne avec scanner (exercice 4Bis)
+		//3 - caractere par caractere avec des conditions(pas le temps)
 		SortedMap<String,Integer> cible = new TreeMap<>();
+
+		Function<String,Optional<List<String>>> splitForMap = s -> {
+			List<String> lst = Collections.emptyList();
+			Pattern cv = Pattern.compile("\\s*([A-Za-z ]+) = (\\d+)\\s*");
+			Matcher mCv = cv.matcher(s);
+			if (mCv.matches() && mCv.groupCount() == 2) {
+				lst = Arrays.asList(mCv.group(1),mCv.group(2));
+				return Optional.ofNullable(lst);
+			}
+			else {
+				System.out.format("aucun matches pour %s\n",s);
+				return Optional.empty();
+			}
+		};
+
+		/*
 		Function<String,Optional<List<String>>> splitForMap = s -> {
 			List<String> lst = Collections.emptyList();
 			String[] cv = s.split("=");
@@ -120,6 +145,7 @@ public class ExFichiers {
 				return Optional.empty();
 			}
 		};
+		*/
 		BiConsumer<String,SortedMap<String,Integer>> strToMap = (s,m) -> {
 			Optional<List<String>> opLst;
 			opLst = splitForMap.apply(s);
@@ -133,10 +159,56 @@ public class ExFichiers {
 			e.printStackTrace();
 		}
 		System.out.println(cible);
+	}
+	public static void exercice4Bis() {
+		SortedMap<String,Integer> cible = new TreeMap<>();
+		try {
+			Scanner sc = new Scanner(Files.newInputStream(source,StandardOpenOption.READ));
+			//sc.findInLine("\\s*([A-Za-z ]+) = (\\d+)\\s*");
+			sc.useDelimiter("\\s*(=|\\n)\\s*");
+			while(sc.hasNext()) {
+				//System.out.println(sc.next().toString()+"->"+sc.nextInt());
+				cible.put(sc.next(),sc.nextInt());
+			}
+			sc.close();
+		
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println(cible);
+	
+	}
+	public static void exercice5() {
+		Couple<Integer> c1 = new Couple<>(1,1), c2 = new Couple<>(2,2), c3 = new Couple<>(3,3);
+		try {
+			OutputStream os = Files.newOutputStream(objetCouple);
+			ObjectOutputStream oos = new ObjectOutputStream(os);
+			oos.writeObject(Arrays.asList(c1,c2,c3));
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	public static void exercice6()  {
+		Optional<List<Couple<Integer>>> lst = Optional.ofNullable(Collections.emptyList());
+		try {
+		InputStream in = Files.newInputStream(objetCouple,StandardOpenOption.READ);
+		ObjectInputStream ois = new ObjectInputStream(in);
+		List<Couple<Integer>> lstCouple = (List<Couple<Integer>>) ois.readObject();
+		lst = Optional.ofNullable(lstCouple);
+		ois.close();
+		
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+		catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+lst.ifPresent(v -> System.out.println(v));
 
-
-
-
+	
 	}
 	public static void main(String[] args) {
 		Pres.titre("Les Fichiers");
@@ -145,5 +217,8 @@ public class ExFichiers {
 		exercice2();
 		exercice3();
 		exercice4();
+		exercice4Bis();
+		exercice5();
+		exercice6();
 	}
 }
