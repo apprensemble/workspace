2ce529101795d3bbe8d458d5d303aa2d9d1edeb4
thierry@moncourriel.eu <thierry>
2016-02-28 20:27:41 +0100

    Observer
    Dans le cours il est pourtant marqué que la declaration fonctionnelle du
    SetChangeListener n'est pas possible et j'ai quand même essayé. Je
    croyais que ce n'était pas conseillé.
    1 heure de perdu!

--- Observer/src/observable/ExObserver.java
+++ Observer/src/observable/ExObserver.java
@@ -1,10 +1,17 @@
 package observable;
 
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableSet;
+import javafx.collections.SetChangeListener;
+
+import json.City;
 
 public class ExObserver {
-	public static void exercice1() {
+	private static void exercice1() {
 		PremierEssai pe = new PremierEssai();
 		pe.nomProperty("Thirry?");
 		pe.nomProperty("Thierry!");
@@ -12,9 +19,38 @@ public class ExObserver {
 		pe.nomProperty("Thierry");
 		
 	}
+	private static void exercice1Bis() {
+		//faisons simple
+		StringProperty sp = new SimpleStringProperty();
+		sp.addListener((o,v,nv) -> System.out.println(o));
+		BooleanProperty bp = new SimpleBooleanProperty();
+		bp.addListener((o,v,nv) -> System.out.format("%s -> %s\n",v,nv));
+		sp.setValue("coucou");
+		bp.setValue(true);
+		bp.setValue(true);
+		bp.setValue(false);
+	
+	}
+	private static void exercice2() {
+		ObservableSet<City> oc = FXCollections.observableSet();
+		//oc.addListener(change -> System.out.format(change.wasAdded() ? "ajout de %s" : "retrait de %s",change.getElementAdded(),change.getElementRemoved()));
+/*	java un langage pour les devins	
+ *		oc.addListener(change -> {
+			if (change.wasAdded()) {
+				System.out.println(change.getElementAdded().toString());
+			}
+		});*/
+
+		//finalement on va faire comme le prof
+		SetChangeListener<City> stl = change -> System.out.println("ajout de "+change.getElementAdded()+" - retrait de "+change.getElementRemoved());
+		oc.addListener(stl);
+		oc.add(new City("paris",200));
+		oc.forEach(f -> oc.remove(f));
+	}
 	public static void main(String[] args) {
 		exercice1();
-	
+		exercice1Bis();
+		exercice2();
 	}
 
 }
