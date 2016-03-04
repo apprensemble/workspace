ae2ecb1c28d9ec385c4561df4d403b961ee1c10e
thierry@moncourriel.eu <thierry>
2016-03-04 07:31:13 +0100

    JavaFX
    Je me suis surtout organisé. Je n'ai rien fais de concret.

--- JavaFX/src/gui/controller/SensorApplicationController.java
+++ JavaFX/src/gui/controller/SensorApplicationController.java
@@ -14,5 +14,6 @@ public class SensorApplicationController {
 		super();
 		sam = new SensorApplicationModel();
 		this.sav = sav;
+		
 	}
 }
--- JavaFX/src/gui/model/SensorApplicationModel.java
+++ JavaFX/src/gui/model/SensorApplicationModel.java
@@ -1,4 +1,7 @@
 package gui.model;
 
+import javafx.beans.property.ObjectProperty;
+
 public class SensorApplicationModel {
+	private ObjectProperty<Sensor> sensor;
 }
